a6a954add299a3cc38759c220d9fdf062b894531
ohjh9701 <ohjh4875@gmail.com>
2026-04-09 17:32:09 +0900

    배포용 URL 수정

--- ggultem/src/api/config.js
+++ ggultem/src/api/config.js
@@ -1,4 +1,4 @@
 // src/api/config.js
 //export const API_SERVER_HOST = "http://localhost:8080";
-export const API_SERVER_HOST = "http://52.78.129.220:8080";
+export const API_SERVER_HOST = "http://52.78.129.220";
 
